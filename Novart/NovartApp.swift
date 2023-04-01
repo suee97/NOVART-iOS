303732040d7a64d413db635c09afce7efee7ac46
Jinwook Huh <gjwlsdnr0115@gmail.com>
2023-04-01 15:25:26 +0900

    Initial Project Commit

---
+++ Novart/NovartApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovartApp.swift
+//  Novart
+//
+//  Created by Jinwook Huh on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct NovartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
