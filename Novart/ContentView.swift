fbdcb9acd578c4702811df82ea6fcfd6d58d74f5
Jinwook Huh <gjwlsdnr0115@gmail.com>
2023-04-01 15:25:26 +0900

    Initial Project Commit

---
+++ Novart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Novart
+//
+//  Created by Jinwook Huh on 2023/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
