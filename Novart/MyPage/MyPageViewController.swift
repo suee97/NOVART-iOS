41dfe778ea2fbf49c30709e0419497904b50ac6b
suee97 <atn1su@gmail.com>
2023-09-06 19:09:11 +0900

    feat: 마이페이지 네비게이션바 UI

--- Novart/MyPage/MyPageViewController.swift
+++ Novart/MyPage/MyPageViewController.swift
@@ -1,29 +1,44 @@
-//
-//  MyPageViewController.swift
-//  Novart
-//
-//  Created by Jinwook Huh on 2023/07/06.
-//
-
 import UIKit
 
-class MyPageViewController: UIViewController {
+final class MyPageViewController: BaseViewController {
+    
+    // MARK: - UI
+    override func setupNavigationBar() {
+        let iconSize = CGRect(origin: CGPoint.zero, size: CGSize(width: 24, height: 24))
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        let notificationButton = UIButton(frame: iconSize)
+        notificationButton.setBackgroundImage(UIImage(named: "icon_notification2"), for: .normal) // 기존 icon_notification이 존재해서 숫자 2를 붙임. 기존 아이콘 사용 안하는거면 수정이 필요합니다
+        notificationButton.addTarget(self, action: #selector(onTapNotification), for: .touchUpInside)
+        let notificationItem = UIBarButtonItem(customView: notificationButton)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 16 // figma에서는 20인데, 기본으로 들어가는 space가 있어서 16으로 함
+        
+        let settingButton = UIButton(frame: iconSize)
+        settingButton.setBackgroundImage(UIImage(named: "icon_setting"), for: .normal)
+        settingButton.addTarget(self, action: #selector(onTapSetting), for: .touchUpInside)
+        let settingItem = UIBarButtonItem(customView: settingButton)
 
-        view.backgroundColor = .green
+        let meatballsButton = UIButton(frame: iconSize)
+        meatballsButton.setBackgroundImage(UIImage(named: "icon_meatballs"), for: .normal)
+        meatballsButton.addTarget(self, action: #selector(onTapMeatballs), for: .touchUpInside)
+        let meatballsItem = UIBarButtonItem(customView: meatballsButton)
+        
+        self.navigationItem.rightBarButtonItems = [settingItem, spacer, notificationItem]
+        self.navigationItem.leftBarButtonItem = meatballsItem
+    }
+
+    
+    // MARK: - Selectors
+    @objc private func onTapNotification() {
+        print("Notification Button Tapped")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func onTapSetting() {
+        print("Setting Button Tapped")
     }
-    */
-
+    
+    @objc private func onTapMeatballs() {
+        print("Meatballs Button Tapped")
+    }
 }
