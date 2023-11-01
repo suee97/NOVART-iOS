73673043fb70909501051cf340652f9337f3d1c2
suee97 <atn1su@gmail.com>
2023-11-01 19:48:34 +0900

    feat: 전시 메인화면UI 구현

---
+++ Novart/Exhibition/ExhibitionButtonsView.swift
@@ -0,0 +1,323 @@
+import UIKit
+import SnapKit
+import Combine
+
+final class ExhibitionButtonsView: UIView {
+    
+    // MARK: - Constants
+    private enum Constants {
+        static let backgroundColor = UIColor.white.withAlphaComponent(0.65)
+        static let spacing: CGFloat = 8
+        
+        enum Common {
+            static let font = UIFont.systemFont(ofSize: 16, weight: .regular)
+            static let textColor = UIColor.Common.warmGrey04
+            static let cornerRadius: CGFloat = 18
+        }
+        
+        enum LikeView {
+            enum ImageView {
+                static let leftMargin: CGFloat = 12
+                static let diameter: CGFloat = 24
+            }
+            
+            enum Label {
+                static let leftMargin: CGFloat = 4
+            }
+        }
+        
+        enum CommentView {
+            enum ImageView {
+                static let leftMargin: CGFloat = 12
+                static let diameter: CGFloat = 24
+            }
+            
+            enum Label {
+                static let leftMargin: CGFloat = 4
+            }
+        }
+        
+        enum ShareView {
+            static let width: CGFloat = 80
+            
+            enum ImageView {
+                static let width: CGFloat = 24
+                static let height: CGFloat = 24
+                static let leftMargin: CGFloat = 12
+            }
+            
+            enum Label {
+                static let text: String = "공유"
+                static let leftMargin: CGFloat = 4
+            }
+        }
+        
+        enum GuideView {
+            static let text: String = "전시 안내"
+            static let width: CGFloat = 83
+        }
+    }
+    
+    // MARK: - Properties
+    private var viewModel: ExhibitionViewModel
+    private var cancellables = Set<AnyCancellable>()
+    
+    
+    // MARK: - LifeCycle
+    init(viewModel: ExhibitionViewModel) {
+        self.viewModel = viewModel
+        super.init(frame: .zero)
+        setUpView()
+        setUpBindings()
+        setUpGesture()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpBindings() {
+        viewModel.$cellIndex.sink(receiveValue: { value in
+            
+            // LikeView
+            self.likeCountLabel.text = self.convertCountToString(count: self.viewModel.processedExhibitions[value].likeCount)
+            self.likeView.snp.removeConstraints()
+            let likeViewWidth = self.likeCountLabel.bounds.width + 52
+            self.likeView.snp.makeConstraints({ m in
+                m.width.equalTo(likeViewWidth)
+            })
+            
+            // LikeHeart
+            self.likeHeartImageView.image = self.viewModel.processedExhibitions[value].isLike ? UIImage(named: "icon_exhibition_heart") : UIImage(named: "icon_exhibition_heart_fill")
+            
+            // CommentView
+            self.commentCountLabel.text = self.convertCountToString(count: self.viewModel.processedExhibitions[value].commentCount)
+            self.commentView.snp.removeConstraints()
+            let commenViewWidth = self.commentCountLabel.bounds.width + 52
+            self.commentView.snp.makeConstraints({ m in
+                m.width.equalTo(commenViewWidth)
+            })
+            
+        }).store(in: &cancellables)
+    }
+    
+    // MARK: - UI
+    private lazy var likeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Constants.backgroundColor
+        view.layer.cornerRadius = Constants.Common.cornerRadius
+        
+        view.addSubview(likeHeartImageView)
+        view.addSubview(likeCountLabel)
+        
+        likeHeartImageView.snp.makeConstraints({ m in
+            m.centerY.equalToSuperview()
+            m.left.equalToSuperview().inset(Constants.LikeView.ImageView.leftMargin)
+            m.width.height.equalTo(Constants.LikeView.ImageView.diameter)
+        })
+        
+        likeCountLabel.snp.makeConstraints({ m in
+            m.centerY.equalToSuperview()
+            m.left.equalTo(likeHeartImageView.snp.right).offset(Constants.LikeView.Label.leftMargin)
+        })
+        
+        return view
+    }()
+    
+    private var likeHeartImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    private let likeCountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Constants.Common.textColor
+        label.font = Constants.Common.font
+        return label
+    }()
+    
+    private lazy var commentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Constants.backgroundColor
+        view.layer.cornerRadius = Constants.Common.cornerRadius
+
+        let imageView = UIImageView(image: UIImage(named: "icon_exhibition_comment"))
+
+        view.addSubview(imageView)
+        view.addSubview(commentCountLabel)
+
+        imageView.snp.makeConstraints({ m in
+            m.centerY.equalToSuperview()
+            m.left.equalToSuperview().inset(Constants.CommentView.ImageView.leftMargin)
+            m.width.height.equalTo(Constants.CommentView.ImageView.diameter)
+        })
+
+        commentCountLabel.snp.makeConstraints({ m in
+            m.centerY.equalToSuperview()
+            m.left.equalTo(imageView.snp.right).offset(Constants.CommentView.Label.leftMargin)
+        })
+        
+        return view
+    }()
+    
+    private let commentCountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Constants.Common.textColor
+        label.font = Constants.Common.font
+        return label
+    }()
+    
+    private let shareView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Constants.backgroundColor
+        view.layer.cornerRadius = Constants.Common.cornerRadius
+        
+        let imageView = UIImageView(image: UIImage(named: "icon_exhibition_share"))
+        let label = UILabel()
+        label.text = Constants.ShareView.Label.text
+        label.font = Constants.Common.font
+        label.textColor = Constants.Common.textColor
+        
+        view.addSubview(imageView)
+        view.addSubview(label)
+        
+        imageView.snp.makeConstraints({ m in
+            m.centerY.equalToSuperview()
+            m.left.equalToSuperview().inset(Constants.ShareView.ImageView.leftMargin)
+            m.width.equalTo(Constants.ShareView.ImageView.height)
+            m.height.equalTo(Constants.ShareView.ImageView.height)
+        })
+        
+        label.snp.makeConstraints({ m in
+            m.centerY.equalToSuperview()
+            m.left.equalTo(imageView.snp.right).offset(Constants.ShareView.Label.leftMargin)
+        })
+        
+        return view
+    }()
+    
+    private let guideView: UIView = {
+        let view = UIView()
+        view.backgroundColor = Constants.backgroundColor
+        view.layer.cornerRadius = Constants.Common.cornerRadius
+        
+        let label = UILabel()
+        label.text = Constants.GuideView.text
+        label.textColor = Constants.Common.textColor
+        label.font = Constants.Common.font
+        
+        view.addSubview(label)
+        label.snp.makeConstraints({ m in
+            m.center.equalToSuperview()
+        })
+        
+        return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [likeView, commentView, shareView, guideView])
+        
+        shareView.snp.makeConstraints({ m in
+            m.width.equalTo(Constants.ShareView.width)
+        })
+        
+        guideView.snp.makeConstraints({ m in
+            m.width.equalTo(Constants.GuideView.width)
+        })
+        
+        stackView.spacing = Constants.spacing
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+    
+    private func setUpView() {
+        addSubview(stackView)
+        
+        stackView.snp.makeConstraints({ m in
+            m.top.bottom.centerX.equalToSuperview()
+        })
+    }
+    
+    
+    // MARK: - Functions & Selectors
+    private func convertCountToString(count: Int) -> String {
+        if count > 999 && count < 10000 {
+            let num = count / 100
+            return "\(num/10).\(num%10)천"
+        }
+        
+        if count >= 10000 && count < 100000 {
+            let num = count / 1000
+            return "\(num/10).\(num%10)만"
+        }
+        
+        if count >= 100000 {
+            return "\(count/10000)만"
+        }
+        
+        return String(count)
+    }
+    
+    private func setUpGesture() {
+        let likeGesture = UILongPressGestureRecognizer(target: self, action: #selector(onTapLikeView))
+        let commentGesture = UILongPressGestureRecognizer(target: self, action: #selector(onTapCommentView))
+        let shareGesture = UILongPressGestureRecognizer(target: self, action: #selector(onTapShareView))
+        let guideGesture = UILongPressGestureRecognizer(target: self, action: #selector(onTapGuideView))
+        
+        likeGesture.minimumPressDuration = 0
+        commentGesture.minimumPressDuration = 0
+        shareGesture.minimumPressDuration = 0
+        guideGesture.minimumPressDuration = 0
+        
+        likeView.addGestureRecognizer(likeGesture)
+        commentView.addGestureRecognizer(commentGesture)
+        shareView.addGestureRecognizer(shareGesture)
+        guideView.addGestureRecognizer(guideGesture)
+    }
+    
+    @objc private func onTapLikeView(_ sender: UIGestureRecognizer) {
+        if sender.state == .began {
+            setDimEffect(view: likeView, isPressed: true)
+        } else if sender.state == .ended {
+            setDimEffect(view: likeView, isPressed: false)
+            print("onTapLikeView")
+        }
+    }
+    
+    @objc private func onTapCommentView(_ sender: UIGestureRecognizer) {
+        if sender.state == .began {
+            setDimEffect(view: commentView, isPressed: true)
+        } else if sender.state == .ended {
+            setDimEffect(view: commentView, isPressed: false)
+            print("onTapCommentView")
+        }
+    }
+    
+    @objc private func onTapShareView(_ sender: UIGestureRecognizer) {
+        if sender.state == .began {
+            setDimEffect(view: shareView, isPressed: true)
+        } else if sender.state == .ended {
+            setDimEffect(view: shareView, isPressed: false)
+            print("onTapShareView")
+        }
+    }
+    
+    @objc private func onTapGuideView(_ sender: UIGestureRecognizer) {
+        if sender.state == .began {
+            setDimEffect(view: guideView, isPressed: true)
+        } else if sender.state == .ended {
+            setDimEffect(view: guideView, isPressed: false)
+            print("onTapGuideView")
+        }
+    }
+    
+    private func setDimEffect(view: UIView, isPressed: Bool) {
+        if isPressed {
+            view.backgroundColor = .black.withAlphaComponent(0.1)
+        } else {
+            view.backgroundColor = Constants.backgroundColor
+        }
+    }
+}
---
+++ Novart/Exhibition/ExhibitionViewController.swift
@@ -0,0 +1,202 @@
+import UIKit
+import SnapKit
+import Combine
+
+final class ExhibitionViewController: BaseViewController {
+    
+    // MARK: - Constants
+    private enum Constants {
+        
+        static let animateDuration: CGFloat = 0.2
+        
+        enum CollectionView {
+            static let itemWidth: CGFloat = 342
+            static let itemHeight: CGFloat = 616
+            static let groupSpacing: CGFloat = 16
+            
+            static let topMargin: CGFloat = 8
+            static let height: CGFloat = 616 + 4 // Cell 그림자 고려
+        }
+        
+        enum PageControl {
+            static let currentIndicatorColor = UIColor.Common.warmBlack
+            static let indicatorColor = UIColor.Common.warmBlack.withAlphaComponent(0.2)
+            static let transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+            
+            static let topMargin: CGFloat = 12 - 4 // Cell 그림자 고려
+            static let height: CGFloat = 4
+        }
+        
+        enum ButtonsView {
+            static let topMargin: CGFloat = 20
+            static let height: CGFloat = 36
+        }
+    }
+    
+    
+    // MARK: - Properties
+    private let viewModel: ExhibitionViewModel
+    private var cancellables = Set<AnyCancellable>()
+    
+    
+    // MARK: - LifeCycle
+    init(viewModel: ExhibitionViewModel) {
+        self.viewModel = viewModel
+        super.init()
+        setupCollectionView()
+        viewModel.getExhibitions()
+    }
+    
+    required init? (coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setupBindings() {
+        viewModel.$processedExhibitions.sink(receiveValue: { value in
+            self.collectionView.reloadData()
+            self.pageControl.numberOfPages = value.count
+        }).store(in: &cancellables)
+        
+        viewModel.$cellIndex.sink(receiveValue: { value in
+            self.pageControl.currentPage = value
+            
+            // 배경 색상 변경
+            UIView.animate(withDuration: Constants.animateDuration, animations: {
+                self.view.backgroundColor = self.viewModel.processedExhibitions[value].backgroundColor
+            })
+            
+            // 투명도 변경
+            if let visibleCells = self.collectionView.visibleCells as? [ExhibitionCell],
+               let currentCell = self.collectionView.cellForItem(at: [0, value]) as? ExhibitionCell {
+                visibleCells.forEach({
+                    $0.container.layer.opacity = 0.3
+                })
+                currentCell.container.layer.opacity = 1.0
+            }
+            
+        }).store(in: &cancellables)
+    }
+    
+    
+    // MARK: - UI
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createCollectionViewLayout())
+        collectionView.bounces = false
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
+    
+    private lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = 0
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = Constants.PageControl.indicatorColor
+        pageControl.currentPageIndicatorTintColor = Constants.PageControl.currentIndicatorColor
+        pageControl.transform = Constants.PageControl.transform // indicator 크기
+        return pageControl
+    }()
+    
+    private lazy var buttonsView = ExhibitionButtonsView(viewModel: viewModel)
+    
+    override func setupView() {
+        view.backgroundColor = .gray
+        
+        view.addSubview(collectionView)
+        view.addSubview(pageControl)
+        view.addSubview(buttonsView)
+        
+        collectionView.snp.makeConstraints({ m in
+            m.left.right.equalToSuperview()
+            m.top.equalTo(view.safeAreaLayoutGuide).inset(Constants.CollectionView.topMargin)
+            m.height.equalTo(Constants.CollectionView.height)
+        })
+        
+        pageControl.snp.makeConstraints({ m in
+            m.centerX.equalToSuperview()
+            m.top.equalTo(collectionView.snp.bottom).offset(Constants.PageControl.topMargin)
+            m.height.equalTo(Constants.PageControl.height)
+            m.width.equalToSuperview()
+        })
+        
+        buttonsView.snp.makeConstraints({ m in
+            m.left.right.equalToSuperview()
+            m.top.equalTo(pageControl.snp.bottom).offset(Constants.ButtonsView.topMargin)
+            m.height.equalTo(Constants.ButtonsView.height)
+        })
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+}
+
+
+// MARK: - CollectionView
+extension ExhibitionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+        
+    private func createCollectionViewLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(Constants.CollectionView.itemWidth),
+            heightDimension: .absolute(Constants.CollectionView.itemHeight)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(Constants.CollectionView.itemWidth),
+            heightDimension: .absolute(Constants.CollectionView.itemHeight)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.interGroupSpacing = Constants.CollectionView.groupSpacing
+        
+        
+        /* reference https://stackoverflow.com/questions/18649920/uicollectionview-current-visible-cell-index
+        */
+        section.visibleItemsInvalidationHandler = { [weak self] visibleItems, point, env in
+            
+            // 좌우로 cell 스크롤 시 viewModel.cellIndex 변경
+            guard let self = self else { return }
+            let visibleRect = CGRect(origin: collectionView.contentOffset, size: collectionView.bounds.size)
+            let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+            let visibleIndexPath = collectionView.indexPathForItem(at: visiblePoint)
+            if let index = visibleIndexPath?.row {
+                self.viewModel.cellIndex = index
+            }
+        }
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(ExhibitionCell.self, forCellWithReuseIdentifier: ExhibitionCell.reuseIdentifier)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.processedExhibitions.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExhibitionCell.reuseIdentifier, for: indexPath) as? ExhibitionCell else { return UICollectionViewCell() }
+        cell.exhibition = viewModel.processedExhibitions[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        viewModel.cellIndex = viewModel.cellIndex
+    }
+}
+
