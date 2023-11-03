7ab59d60adf7683729fe347f61ba0ba5dc296a36
suee97 <atn1su@gmail.com>
2023-11-03 10:59:50 +0900

    feat: 마이페이지 -> 마이페이지 설정 네비게이션 구현

--- Novart/MyPage/MyPageSetting/MyPageSettingViewController.swift
+++ Novart/MyPage/MyPageSetting/MyPageSettingViewController.swift
@@ -74,13 +74,22 @@ final class MyPageSettingViewController: BaseViewController {
     
     
     // MARK: - Properties
-    private let viewModel = MyPageSettingViewModel()
+    private let viewModel: MyPageSettingViewModel
     
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
+    }
+    
+    init(viewModel: MyPageSettingViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - UI
--- Novart/MyPage/MyPageSetting/MyPageSettingViewModel.swift
+++ Novart/MyPage/MyPageSetting/MyPageSettingViewModel.swift
@@ -2,5 +2,17 @@ import UIKit
 import Combine
 
 final class MyPageSettingViewModel {
+    // MARK: - Properties
+    private let coordinator: MyPageCoordinator
     
+    init(coordinator: MyPageCoordinator) {
+        self.coordinator = coordinator
+    }
+    
+    
+    // MARK: - Navigation
+    @MainActor
+    func showMain() {
+        coordinator.navigate(to: .MyPageSetting)
+    }
 }
