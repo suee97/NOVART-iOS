251ac243338756798d7a2b5b721025e345bd93a1
Jinwook Huh <gjwlsdnr0115@gmail.com>
2024-02-17 21:45:01 +0900

    feat: PlainSnackBar 추가

--- Novart/PresentationLayer/Block/BlockViewModel.swift
+++ Novart/PresentationLayer/Block/BlockViewModel.swift
@@ -33,6 +33,7 @@ extension BlockViewModel {
 //                try await cleanInteractor.makeBlockRequest(userId: user.id)
                 DispatchQueue.main.async { [weak self] in
                     self?.closeCoordinator()
+                    PlainSnackbar.show(message: "이 사용자를 차단했어요.", configuration: .init(imageType: .icon(.block)))
                 }
             } catch {
                 print(error.localizedDescription)
