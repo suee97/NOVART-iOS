e1d1b58c25a18b2f03c759323a04ca31c4705e6b
Jinwook Huh <gjwlsdnr0115@gmail.com>
2024-03-22 18:44:53 +0900

    NotificationNavigation 관련 코드 추가

---
+++ Novart/App/NotificationNavigation/NotificationNavigationFactory.swift
@@ -0,0 +1,27 @@
+//
+//  NotificationNavigationFactory.swift
+//  Novart
+//
+//  Created by Jinwook Huh on 3/22/24.
+//
+
+import UIKit
+
+struct NotificationNavigationFactory {
+    static func create(notification: NotificationModel) -> NotificationNavigationExecutable? {
+        switch notification.type {
+        case .Follow:
+            return NotificationProfileAction(notification: notification)
+        case .Likes:
+            return NotificationProductAction(notification: notification)
+        case .Comment:
+            return NotificationProductAction(notification: notification)
+        case .Welcome:
+            return nil
+        case .Register:
+            return NotificationProductAction(notification: notification)
+        case .None:
+            return nil
+        }
+    }
+}
