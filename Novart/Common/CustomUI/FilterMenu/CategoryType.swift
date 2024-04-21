c8ced8f9edeaf09c49e8aa1a985278c797ed0351
suee97 <atn1su@gmail.com>
2024-04-21 14:55:25 +0900

    feat: 홈 카테고리 순서 변경 및 작품 업로드 카테고리 순서 변경

--- Novart/Common/CustomUI/FilterMenu/CategoryType.swift
+++ Novart/Common/CustomUI/FilterMenu/CategoryType.swift
@@ -9,9 +9,9 @@ import Foundation
 
 enum CategoryType: String, CaseIterable, Decodable {
     case all = "전체"
-    case product = "제품"
-    case craft = "공예"
     case painting = "회화"
+    case craft = "공예"
     case graphic = "그래픽"
+    case product = "리빙"
     case fashion = "패션"
 }
