e05039e55e4fb075ddac077ca9a52c2c547a6021
suee97 <atn1su@gmail.com>
2024-08-12 15:53:33 +0900

    refactor: 마이페이지 메인에서 유저의 모든 카테고리의 모든 정보를 가져오는 기능 리팩토링

--- Novart/DomainLayer/RepositoryInterface/MyPageRepositoryInterface.swift
+++ Novart/DomainLayer/RepositoryInterface/MyPageRepositoryInterface.swift
@@ -2,4 +2,5 @@ import Foundation
 
 protocol MyPageRepositoryInterface {
     func fetchNotificationCheckStatus() async throws -> NotificationCheckStatus
+    func fetchAllCategoryContents(userId: Int64) async throws -> (interests: [ProductModel], followings: [ArtistModel], works: [MyPageWork], exhibitions: [ExhibitionModel])
 }
