2191882735f659282e40c0acc57836c9e3b143e0
suee97 <atn1su@gmail.com>
2024-08-13 00:32:39 +0900

    refactor: 마이페이지 메인 팔로우, 언팔로우, 다른유저 정보 불러오는 기능 리팩토링

--- Novart/DomainLayer/RepositoryInterface/MyPageRepositoryInterface.swift
+++ Novart/DomainLayer/RepositoryInterface/MyPageRepositoryInterface.swift
@@ -5,4 +5,7 @@ protocol MyPageRepositoryInterface {
     func fetchAllCategoryContents(userId: Int64) async throws -> (interests: [ProductModel], followings: [ArtistModel], works: [MyPageWork], exhibitions: [ExhibitionModel])
     func fetchRecommendInterests() async throws -> [ProductModel]
     func fetchRecommendFollowings() async throws -> [ArtistModel]
+    func follow(userId: Int64) async throws -> EmptyResponseModel
+    func unFollow(userId: Int64) async throws -> EmptyResponseModel
+    func fetchUserInfo(userId: Int64) async throws -> PlainUser
 }
